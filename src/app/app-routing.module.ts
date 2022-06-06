407d6495da23e05d1e0d1deff34235ae7aa082d4
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-06-06 15:03:14 +0200

    Add courses routes to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomepageComponent} from "./homepage/homepage.component";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) },
   { path: 'instructor', loadChildren: () => import('./instructor/instructor.module').then(m => m.InstructorModule) },
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {HomepageModule} from "./homepage/homepage.module";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {CourseService} from "./instructor/services/course.service";
+import { CoursesModule } from './courses/courses.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import {CourseService} from "./instructor/services/course.service";
     BrowserModule,
     AppRoutingModule,
     HomepageModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    CoursesModule
   ],
   bootstrap: [AppComponent]
 })
